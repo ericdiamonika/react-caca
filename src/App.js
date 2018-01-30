3e0f7c2a34f742b600281649970236b12c56b9c5
greedjow <eric.diamonika@hetic.net>
2018-01-30 10:19:15 +0100

    #react-caca : First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
+            value:'',
             seriesList: [],
             seriesEpisodesList: []
         };
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(event)
+    {
+        this.setState({value: event.target.value});
+    }
     componentDidMount() {
+
 
         fetch('seriesList.json',{})
             .then(response => response.json())
             .then(seriesListDepuisFichier => {
                 this.setState({seriesList: seriesListDepuisFichier});
+
+            });
+        fetch('seriesEpisodesList.json',{})
+            .then(response => response.json())
+            .then(seriesEpisodesListDepuisFichier => {
+                this.setState({seriesEpisodesList: seriesEpisodesListDepuisFichier});
 
             })
             .catch(function (error) {
@@ -31,9 +43,17 @@ class App extends React.Component {
     render() {
         return (
             <div>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
                 <ul>
                     {this.state.seriesList.length ?
                         this.state.seriesList.map(item => <li key={item.id}>{item.seriesName}</li>)
+                        : <li>Loading...</li>
+                    }
+                    {this.state.seriesEpisodesList.length ?
+                        this.state.seriesEpisodesList.map(item => <li key={item.serie_id}>{item.episodeName}</li>)
                         : <li>Loading...</li>
                     }
                 </ul>
