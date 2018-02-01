5beb1966175820738d159103cc8ca9352ae6a202
greedjow <eric.diamonika@hetic.net>
2018-02-01 02:46:11 +0100

    #react-caca : pk je dors pas

--- src/App.js
+++ src/App.js
@@ -48,26 +48,36 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <div className="react-caca">
+                <h1 className="title">REACT CACA</h1>
+                <input className="input-caca" placeholder={"Type something"} type={"text"} value={this.state.value} onChange={this.handleChange} />
                 <ul>
                     {this.state.value !== "" ?
                         this.state.seriesList.filter(
+
+                            /*The trim() method removes whitespace from both ends of a string. Whitespace
+                            in this context is all the whitespace characters
+                            (space, tab, no-break space, etc.) and all the line terminator characters
+                            (LF, CR, etc.).*/
+
+                            /*The indexOf() method returns the first index at which a given element
+                            can be found in the array, or -1 if it is not present.*/
+
                             a => a.seriesName.toLowerCase().trim().indexOf(this.state.value) > -1
                         ).map(item =>
-                            <li key={item.id}>{item.seriesName}
+                            <li> <div className="serie-name" key={item.id}>{item.seriesName}</div>
                                 <ul>
                                     {
                                         this.state.seriesEpisodesList.filter(
-                                            b => b.serie_id == item.id
-                                        ).map(episode => episode.episodes_list.filter(
+                                            b => b.serie_id === item.id
+                                        ).map(ListEpisode => ListEpisode.episodes_list.filter(
                                             c => c.episodeName
-                                            ).map(name => <li>{name.episodeName}</li>)
+                                            ).map(ListEpisodeName => <li className="episode-name">{ListEpisodeName.episodeName}</li>)
                                         )
                                     }
                                 </ul>
                             </li>)
-                        : <li>Loading...</li>
+                        : <li>LMGIFY</li>
                     }
                 </ul>
             </div>
